aec419e8b6e0785911babe384e0ff1f6f2960137
namtiennguyen <toilanam97@mail.com>
2019-08-01 09:38:40 +0700

    bai 3

---
+++ dientich.js
@@ -0,0 +1,5 @@
+let r = Number(prompt("Nhập bán kính hình tròn: "));
+let s = (r*r) * 3.14;
+document.write("Bán kính là: " + r);
+document.write("</br>");
+document.write("Diện tích hình tròn là: " + s);
